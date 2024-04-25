5b9a5d7fa8ce28f14abdace8f1137e3fb7afe760
Eber Bonila <bhuuuje@gmail.com>
2024-04-24 20:26:53 -0600

    Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'XD': '#050B28',
+        'C1': '#372894',
+        'C2': '#20258C',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+    },
   },
   plugins: [],
 }
